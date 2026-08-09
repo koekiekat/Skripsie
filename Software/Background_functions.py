e384e09dc74f4af72c1f74f02f948fd1da450e06
Elisabeth Christina Bosch <26894238@sun.ac.za>
2026-08-09 15:40:22 +0200

    DTW try from Kamper code

--- Software/Background_functions.py
+++ Software/Background_functions.py
@@ -26,10 +26,10 @@ def split_audio_segments(x, f_s, start_t, end_t):
         audio_segments.append(audio_segment)
     return audio_segments
 
-def resample_audio(audio_segment, f_s, fs_new):
+def resample_audio(audio_segments, f_s, fs_new):
     up = 1
     down = int(f_s / fs_new)
-    audio_resampled = signal.resample_poly(audio_segment, up, down)
+    audio_resampled = signal.resample_poly(audio_segments, up, down)
     return audio_resampled
 
 def stft_calculation(audio_resampled, f_s, fs_new):
@@ -53,3 +53,61 @@ def call_length_stats(df):
     longest_call_length = np.max(diff)
     shortest_call_length = np.min(diff)
     return average_call_length, longest_call_length, shortest_call_length
+
+def dp(dist_mat):
+    """
+    Find minimum-cost path through matrix `dist_mat` using dynamic programming.
+
+    The cost of a path is defined as the sum of the matrix entries on that
+    path. See the following for details of the algorithm:
+
+    - http://en.wikipedia.org/wiki/Dynamic_time_warping
+    - https://www.ee.columbia.edu/~dpwe/resources/matlab/dtw/dp.m
+
+    The notation in the first reference was followed, while Dan Ellis's code
+    (second reference) was used to check for correctness. Returns a list of
+    path indices and the cost matrix.
+    """
+
+    N, M = dist_mat.shape
+    
+    # Initialize the cost matrix
+    cost_mat = np.zeros((N + 1, M + 1))
+    for i in range(1, N + 1):
+        cost_mat[i, 0] = np.inf
+    for i in range(1, M + 1):
+        cost_mat[0, i] = np.inf
+
+    # Fill the cost matrix while keeping traceback information
+    traceback_mat = np.zeros((N, M))
+    for i in range(N):
+        for j in range(M):
+            penalty = [
+                cost_mat[i, j],      # match (0)
+                cost_mat[i, j + 1],  # insertion (1)
+                cost_mat[i + 1, j]]  # deletion (2)
+            i_penalty = np.argmin(penalty)
+            cost_mat[i + 1, j + 1] = dist_mat[i, j] + penalty[i_penalty]
+            traceback_mat[i, j] = i_penalty
+
+    # Traceback from bottom right
+    i = N - 1
+    j = M - 1
+    path = [(i, j)]
+    while i > 0 or j > 0:
+        tb_type = traceback_mat[i, j]
+        if tb_type == 0:
+            # Match
+            i = i - 1
+            j = j - 1
+        elif tb_type == 1:
+            # Insertion
+            i = i - 1
+        elif tb_type == 2:
+            # Deletion
+            j = j - 1
+        path.append((i, j))
+
+    # Strip infinity edges from cost_mat before returning
+    cost_mat = cost_mat[1:, 1:]
+    return (path[::-1], cost_mat)
